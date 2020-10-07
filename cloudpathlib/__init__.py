8692c596d98292c4ca44fd2e654effb595db1427
Jay Qi <jayqi@users.noreply.github.com>
2020-10-06 20:10:56 -0400

    Add __version__

--- cloudpathlib/__init__.py
+++ cloudpathlib/__init__.py
@@ -1,8 +1,20 @@
+import sys
+
 from .azure.azblobclient import AzureBlobClient
 from .azure.azblobpath import AzureBlobPath
 from .cloudpath import CloudPath
 from .s3.s3client import S3Client
 from .s3.s3path import S3Path
+
+
+if sys.version_info[:2] >= (3, 8):
+    import importlib.metadata as importlib_metadata
+else:
+    import importlib_metadata
+
+
+__version__ = importlib_metadata.version(__name__.split(".", 1)[0])
+
 
 # exceptions
 from .cloudpath import (
