8ed6a893d5b3e73dd4c8fb9138aab5ca6d392d79
Jay Qi <jayqi@users.noreply.github.com>
2020-10-12 19:49:31 -0400

    Add cloudpathlib to test script

--- permissions.py
+++ permissions.py
@@ -1,6 +1,9 @@
 import boto3
 import os
 import shortuuid
+from pathlib import Path
+
+from cloudpathlib import CloudPath
 
 bucket_name = os.getenv("LIVE_S3_BUCKET")
 print("len(bucket_name):", len(bucket_name))
@@ -11,16 +14,19 @@ print("len(secret_access_key):", len(secret_access_key))
 
 
 # Retrieve a bucket's ACL
-session = boto3.Session(
-    aws_access_key_id=access_key_id,
-    aws_secret_access_key=secret_access_key,
-)
+# session = boto3.Session(
+#     aws_access_key_id=access_key_id,
+#     aws_secret_access_key=secret_access_key,
+# )
 # boto3.DEFAULT_SESSION = boto3.Session(
 #     aws_access_key_id=access_key_id,
 #     aws_secret_access_key=secret_access_key,
 # )
-s3 = session.resource("s3")
-bucket = s3.Bucket(bucket_name)
+# s3 = session.resource("s3")
+# bucket = s3.Bucket(bucket_name)
+
+bucket = boto3.resource("s3").Bucket(bucket_name)
+
 
 new_file = f"README-{shortuuid.uuid()}.txt"
 print("New file:", new_file)
@@ -29,6 +35,17 @@ bucket.upload_file("README.md", new_file)
 [print(o) for o in bucket.objects.all()]
 
 
-bucket.objects.filter(Prefix="README").delete()
 print("Deleting")
+bucket.objects.filter(Prefix="README").delete()
 [print(o) for o in bucket.objects.all()]
+
+print("Now cloudpathlib")
+
+cp = CloudPath(f"s3://{bucket_name}") / new_file
+with Path("README.md").open("r") as fp:
+    cp.write_text("blah blah blah")
+[print(o) for o in CloudPath(f"s3://{bucket_name}").iterdir()]
+
+print("Deleting")
+cp.unlink()
+[print(o) for o in CloudPath(f"s3://{bucket_name}").iterdir()]
