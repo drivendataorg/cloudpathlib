1732618eac43e23996529f1d923a30866469077a
Macklan Weinstein <wolfgang.wazzle.strauss@gmail.com>
2021-01-06 23:27:45 -0800

    Implement pass methods for GSClient

--- cloudpathlib/gs/gsclient.py
+++ cloudpathlib/gs/gsclient.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import os
+from pathlib import PurePosixPath
 from typing import Any, Dict, Iterable, Optional, Union
 
 from ..client import Client, register_client_class
@@ -35,7 +36,11 @@ class GSClient(Client):
         bucket = self.client.get_bucket(cloud_path.bucket)
         blob = bucket.get_blob(cloud_path.blob)
 
-        return blob.metadata or {}
+        return {
+            "etag": blob.etag,
+            "size": blob.size,
+            "updated": blob.updated,
+        }
 
     def _download_file(
         self, cloud_path: GSPath, local_path: Union[str, os.PathLike]
@@ -55,7 +60,7 @@ class GSClient(Client):
         bucket = self.client.get_bucket(cloud_path.bucket)
         blob = bucket.get_blob(cloud_path.blob)
 
-        if blob.exists():
+        if blob is not None:
             return "file"
         else:
             prefix = cloud_path.blob
@@ -75,17 +80,56 @@ class GSClient(Client):
         return self._is_file_or_dir(cloud_path) in ["file", "dir"]
 
     def _list_dir(self, cloud_path: GSPath, recursive=False) -> Iterable[GSPath]:
-        pass
+        bucket = self.client.get_bucket(cloud_path.bucket)
+
+        prefix = cloud_path.blob
+        if prefix and not prefix.endswith("/"):
+            prefix += "/"
+
+        yielded_dirs = set()
+
+        # NOTE: Not recursive may be slower than necessary since it just filters
+        #   the recursive implementation
+        for o in bucket.list_blobs(prefix=prefix):
+            # get directory from this path
+            for parent in PurePosixPath(o.name[len(prefix) :]).parents:
+
+                # if we haven't surfaced thei directory already
+                if parent not in yielded_dirs and str(parent) != ".":
+
+                    # skip if not recursive and this is beyond our depth
+                    if not recursive and "/" in str(parent):
+                        continue
+
+                    yield self.CloudPath(f"gs://{cloud_path.bucket}/{prefix}{parent}")
+                    yielded_dirs.add(parent)
+
+            # skip file if not recursive and this is beyond our depth
+            if not recursive and "/" in o.name[len(prefix) :]:
+                continue
+
+            yield self.CloudPath(f"gs://{cloud_path.bucket}/{o.name}")
 
     def _move_file(self, src: GSPath, dst: GSPath) -> GSPath:
         # just a touch, so "REPLACE" metadata
         if src == dst:
             bucket = self.client.bucket(src.bucket)
             blob = bucket.get_blob(src.blob)
-            blob.updated = datetime.utcnow().timestamp()
+
+            # See https://github.com/googleapis/google-cloud-python/issues/1185#issuecomment-431537214
+            if blob.metadata is None:
+                blob.metadata = {"updated": datetime.utcnow()}
+            else:
+                blob.metadata["updated"] = datetime.utcnow()
+            blob.patch()
 
         else:
-            pass
+            src_bucket = self.client.bucket(src.bucket)
+            dst_bucket = self.client.bucket(dst.bucket)
+
+            src_blob = src_bucket.get_blob(src.blob)
+            src_bucket.copy_blob(src_blob, dst_bucket, dst.blob)
+            src_blob.delete()
 
         return dst
 
@@ -94,7 +138,7 @@ class GSClient(Client):
             blobs = [b.blob for b in self._list_dir(cloud_path, recursive=True)]
             bucket = self.client.bucket(cloud_path.bucket)
             for blob in blobs:
-                blob.delete()
+                bucket.delete_blob(blob)
         elif self._is_file_or_dir(cloud_path) == "file":
             bucket = self.client.bucket(cloud_path.bucket)
             bucket.delete_blob(cloud_path.blob)
