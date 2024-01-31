2f0c34c503da05b7b81784d30659091580bcc3a2
Kabir Khan <kabir@explosion.ai>
2024-01-31 12:31:11 -0800

    rm extra commented out

--- cloudpathlib/azure/azblobpath.py
+++ cloudpathlib/azure/azblobpath.py
@@ -66,9 +66,6 @@ class AzureBlobPath(CloudPath):
             url = self.client._generate_presigned_url(self, expire_seconds=expire_seconds)
         else:
             url = self.client._get_public_url(self)
-            # url = f"https://{self.client.service_client.account_name}.blob.core.windows.net/{self.container}"
-            # if self.blob:
-            #     url += f"/{self.blob}"
         return url
 
     def stat(self):
